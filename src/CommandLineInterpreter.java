a3147060b4446b6bd79697e87c097926ae1ea9da
omarse7a <omar.seha90@gmail.com>
2024-10-29 23:02:06 +0300

    added the cd command implementation

--- src/CommandLineInterpreter.java
+++ src/CommandLineInterpreter.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CommandLineInterpreter {
 
     public String pwd(){
         return System.getProperty("user.dir");
+    }
+
+    public void cd(String path){
+        if(!path.equals(this.pwd()) && path.charAt(1) == ':') { // when the given path is absolute path
+            System.setProperty("user.dir", path);
+            return;
+        }
+        // splitting the current path into an ArrayList
+        ArrayList<String> currentPath = new ArrayList<>(Arrays.asList(this.pwd().split("\\\\")));
+        String[] pathDirs = path.split("\\\\"); // splitting the given pass into an array
+        for(String dir : pathDirs) {
+            if(dir.equals("..")) {
+                if(currentPath.size() == 1)
+                    continue;
+                currentPath.remove(currentPath.size() - 1);
+            }
+            else
+                currentPath.add(dir);
+        }
+        String newPath = String.join("\\", currentPath);
+        System.setProperty("user.dir", newPath);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         CommandLineInterpreter cli = new CommandLineInterpreter();
         System.out.println(cli.pwd());
+        cli.cd("..\\LZ77 compression and decompression");
+        System.out.println(cli.pwd());
     }
 }
