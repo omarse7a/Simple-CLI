3f87e7ea3f41ba0237e6f7d952c1db1097528f14
omarse7a <omar.seha90@gmail.com>
2024-10-30 00:58:38 +0300

    fixed the cd and added commands mkdir and rmdir

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         CommandLineInterpreter cli = new CommandLineInterpreter();
         System.out.println(cli.pwd());
-        cli.cd("..\\LZ77 compression and decompression");
+        cli.cd("..");
+        System.out.println(cli.pwd());
+        cli.rmdir("folder1\\folder2");
+        cli.cd("folder1");
         System.out.println(cli.pwd());
     }
 }
