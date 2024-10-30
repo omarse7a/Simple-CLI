b621c7fe99e8f2a795f04f59436c7857ff4114ac
omarse7a <omar.seha90@gmail.com>
2024-10-30 10:56:26 +0300

    updated the main program

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         CommandLineInterpreter cli = new CommandLineInterpreter();
-        System.out.println(cli.pwd());
-        cli.cd("..");
-        System.out.println(cli.pwd());
-        cli.rmdir("folder1\\folder2");
-        cli.cd("folder1");
-        System.out.println(cli.pwd());
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(cli.pwd() + "~ ");
+        while(scanner.hasNextLine()) {
+
+            String input = scanner.nextLine();
+            String[] inputParts = input.split(" "); // potential problem: a path could have spaces!
+            switch(inputParts[0]){
+                case "pwd":
+                    System.out.println(cli.pwd()); break;
+                case "exit()":
+                    System.exit(0); break;
+                default:
+                    System.out.println(inputParts[0] + ": Command does not exist"); break;
+            }
+            System.out.print(cli.pwd() + "~ ");
+
+        }
     }
 }
